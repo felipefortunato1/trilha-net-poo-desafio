c60e65ffcf4abc571c5fc4caf3ac0d922a1b6450
Felipe Fortunato <felipefreitasfortunato@gmail.com>
2024-05-19 19:31:35 -0300

    Desafio Concluídoogit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1681381343", modelo: "FFF111", imei: "184648343", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "987540000", modelo: "FFF222", imei: "13438739", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+
