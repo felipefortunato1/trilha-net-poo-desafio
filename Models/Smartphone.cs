b5b491863c3d89ee0f50c2dae22eacc96eca8fcb
Felipe Fortunato <132859322+felipefortunato1@users.noreply.github.com>
2024-08-29 15:21:39 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,9 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
